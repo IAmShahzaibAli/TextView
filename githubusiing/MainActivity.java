1e9726bccc9a404b93812fe521214d03308b5854
Shahzaib Ali <Contact.ShahzaibAli@gmail.com>
2020-08-11 01:48:47 +0500

    Initial Commit

---
+++ githubusiing/MainActivity.java
@@ -0,0 +1,16 @@
+package com.beautifull.githubusiing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String InitialGithub;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
